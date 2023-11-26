1bdf47c0cc4f0a8483d0a5f982513719363088fe
matt <mattblodgett94@gmail.com>
2023-11-26 04:30:02 -0700

    Refactor

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,17 +1,18 @@
-function reportError(error) {
+function logError(error) {
   console.error(`Error: ${error}`);
 }
 
 function listenForClicks() {
   document.addEventListener("click", (event) => {
-    function test(tabs) {
+    function test() {
       console.log("test from popup.js");
+      document.getElementById('warning-label').textContent = 'Warning';
     }
 
     function popupButtonClicked(tabs) {
       const action = event.target.textContent;
       if (action === "Test1") {
-        test(tabs);
+        test();
       } else if (action === "Test2") {
         browser.tabs
           .sendMessage(tabs[0].id, {
@@ -37,7 +38,7 @@ function listenForClicks() {
   });
 }
 
-function reportExecuteScriptError(error) {
+function handleErrorExecuteScript(error) {
   document.querySelector("#popup-content").classList.add("hidden");
   document.querySelector("#error-content").classList.remove("hidden");
   console.error(`Failed to execute content script: ${error.message}`);
@@ -52,10 +53,10 @@ function injectScripts(tabs) {
       files: ["/scripts/third_party/browser-polyfill.js", "/scripts/test.js"],
     })
     .then(listenForClicks)
-    .catch(reportExecuteScriptError);
+    .catch(handleErrorExecuteScript);
 }
 
 browser.tabs
   .query({ currentWindow: true, active: true })
   .then(injectScripts)
-  .catch(reportError);
+  .catch(logError);
